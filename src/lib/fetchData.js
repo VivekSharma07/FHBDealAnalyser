42fbee1c8f9e84ac5cf9f5af7d2d818ccc07db16
VivekSharma07 <beyondordinary3@gmail.com>
2024-06-04 14:55:41 +0530

    added firebase function with handleSave, but still getting blocked in dev/production pushing to test live server v2

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -5,29 +5,7 @@ import { doc, getDoc} from "firebase/firestore";
 import { db } from "@/firebase/firebaseConfig";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import { app } from "../firebase/firebaseConfig";
-// If you're using a service account key file:
-// const serviceAccount = require('./fhb-deal-analyser-firebase-adminsdk-85699-6ca3fa1818.json');
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
 
-// const firestore = admin.firestore();
-
-// export async function fetchDealAnalyzerData(docId) {
-//     try {
-//         const docRef = firestore.collection("deal-analyser").doc(docId);
-//         const docSnapshot = await docRef.get();
-
-//         if (docSnapshot.exists) {
-//             return docSnapshot.data();
-//         } else {
-//             return null;
-//         }
-//     } catch (error) {
-//         console.error("Error fetching deal analyzer:", error);
-//         throw new Error("Failed to fetch deal analyzer data");
-//     }
-// }
 
 export async function fetchDealAnalyzerData(docId) {
   try {
